e55535fb0b60ad9c8f77b59c821c8cb7629c5b4c
kiddin9 <48883331+kiddin9@users.noreply.github.com>
2024-08-19 00:34:33 +0800

    update

--- devices/rockchip_rk35xx/diy.sh
+++ devices/rockchip_rk35xx/diy.sh
@@ -3,15 +3,16 @@
 shopt -s extglob
 SHELL_FOLDER=$(dirname $(readlink -f "$0"))
 
-rm -rf package/boot package/feeds/kiddin9/accel-ppp package/devel/perf package/devel/kselftests-bpf package/network/utils/uqmi package/feeds/routing/batman-adv package/kernel/mali-csf package/network/config/qosify
+rm -rf package/boot package/feeds/kiddin9/accel-ppp
+#rm -rf package/devel/perf package/devel/kselftests-bpf package/network/utils/uqmi package/feeds/routing/batman-adv package/kernel/mali-csf package/network/config/qosify
 
 rm -rf target/linux/generic/!(*-5.15) target/linux/rockchip package/kernel
 
-git_clone_path istoreos-22.03 https://github.com/istoreos/istoreos package/boot target/linux/rockchip package/kernel
+git_clone_path master https://github.com/coolsnowwolf/lede package/boot target/linux/rockchip package/kernel
 
-git_clone_path istoreos-22.03 https://github.com/istoreos/istoreos mv target/linux/generic
+git_clone_path master https://github.com/coolsnowwolf/lede mv target/linux/generic
 
-wget -N https://github.com/istoreos/istoreos/raw/istoreos-22.03/include/kernel-5.10 -P include/
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/kernel-5.10 -P include/
 
 cp -rf target/linux/rockchip/rk35xx/files/include target/linux/rockchip/files/
 
@@ -22,14 +23,8 @@ mv -f tmp/r8125 feeds/kiddin9/
 sed -i -e 's,kmod-r8168,kmod-r8169,g' target/linux/rockchip/image/rk35xx.mk
 sed -i -e 's,wpad-openssl,wpad-basic-mbedtls,g' target/linux/rockchip/image/rk35xx.mk
 
-wget -N https://github.com/istoreos/istoreos/raw/istoreos-22.03/include/netfilter.mk -P include/
+wget -N https://github.com/coolsnowwolf/lede/raw/master/include/netfilter.mk -P include/
 
-rm -rf target/linux/generic/hack-5.10/953-net-fix-netns_ct-abi-v2.patch
-
-wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/kernel/linux/modules/video.mk -P package/kernel/linux/modules/
-
-wget -N https://github.com/coolsnowwolf/lede/raw/master/target/linux/generic/hack-5.10/953-net-patch-linux-kernel-to-support-shortcut-fe.patch -P target/linux/generic/hack-5.10/
-wget -N https://raw.githubusercontent.com/coolsnowwolf/lede/master/target/linux/generic/pending-5.10/613-netfilter_optional_tcp_window_check.patch -P target/linux/generic/pending-5.10/
 
 sed -i 's/DEFAULT_PACKAGES +=/DEFAULT_PACKAGES += fdisk lsblk kmod-drm-rockchip/' target/linux/rockchip/Makefile
 
